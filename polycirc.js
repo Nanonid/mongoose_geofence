4694b9b843bcf6cbcfbafc9e5915e691c6e6bb64
Nanonid <eric.lindahl.tab@gmail.com>
2016-07-07 16:44:33 -0400

    error in parameters (fixed at 8)

--- polycirc.js
+++ polycirc.js
@@ -4,10 +4,16 @@ function circumscribedRadius( radius, nsides ){
   return radius * (1.0 / (Math.cos(Math.PI/nsides)));
 }
 
+/**
+ * Create a circumscribed polygon of nsides centered on point, with radius
+ * @param {Array coordinates} point (in lat/lon)
+ * @param {Number} radius in KM
+ * @param {Number} number of sides from 3 to ?
+ */
 function polygonCircumscribe( point, radius, nsides ){
   var polygon = { type: "Polygon" };
   var coordinates = [];
-  var cradius = circumscribedRadius(radius,8);
+  var cradius = circumscribedRadius(radius,nsides);
   var destination = GreatCircle.destination;
   var anglePerSide = 360.0/nsides;
   for( var side = 0; side < nsides; side++ ){
